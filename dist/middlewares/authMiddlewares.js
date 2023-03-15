3bdc31b382ca04119924bfa782ac0c4afa66d239
AdmiJW <jamweg22@gmail.com>
2023-03-15 12:26:34 +0800

    JW: Order Status

--- dist/middlewares/authMiddlewares.js
+++ dist/middlewares/authMiddlewares.js
@@ -1,9 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuthenticated = void 0;
+exports.isPlanter = exports.isApe = exports.isCustomer = exports.isAuthenticated = void 0;
 function isAuthenticated(req, res, next) {
     if (!req.isAuthenticated())
         return res.status(401).json({ error: "Not logged in" });
     next();
 }
 exports.isAuthenticated = isAuthenticated;
+function isCustomer(req, res, next) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "CUSTOMER")
+        return res.status(403).json({ error: "Not authorized. You must be a CUSTOMER to use this endpoint" });
+    next();
+}
+exports.isCustomer = isCustomer;
+function isApe(req, res, next) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "APE")
+        return res.status(403).json({ error: "Not authorized. You must be an APE to use this endpoint" });
+    next();
+}
+exports.isApe = isApe;
+function isPlanter(req, res, next) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "PLANTER")
+        return res.status(403).json({ error: "Not authorized. You must be a PLANTER to use this endpoint" });
+    next();
+}
+exports.isPlanter = isPlanter;
--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -9,3 +9,30 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
     next();
 }
 
+
+export function isCustomer(req: Request, res: Response, next: NextFunction) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "CUSTOMER")
+        return res.status(403).json({ error: "Not authorized. You must be a CUSTOMER to use this endpoint" });
+    next();
+}
+
+
+export function isApe(req: Request, res: Response, next: NextFunction) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "APE")
+        return res.status(403).json({ error: "Not authorized. You must be an APE to use this endpoint" });
+    next();
+}
+
+
+export function isPlanter(req: Request, res: Response, next: NextFunction) {
+    if (!req.isAuthenticated())
+        return res.status(401).json({ error: "Not logged in" });
+    if (req.user.role !== "PLANTER")
+        return res.status(403).json({ error: "Not authorized. You must be a PLANTER to use this endpoint" });
+    next();
+}
+
