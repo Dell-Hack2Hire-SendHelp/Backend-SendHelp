f2e810a6a7e6d7188415eddfc60364a6a53e9c38
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2023-03-16 08:35:57 +0800

    ZK: Updated CORS Origin

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ const corsOptions = {
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:4173",
+        "http://127.0.0.1:4173",
     ],
     credentials: true,
     exposedHeaders: ['set-cookie'],
